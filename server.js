f8878e2868745cd72fb1a9e2f97dd61f18b889e8
Jakub Plecháč <plechac@spstrutnov.cz>
2023-02-20 22:13:32 +0100

    Generace náhodného textu

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import { Configuration, OpenAIApi } from "openai";
+import dotenv from "dotenv";
 
 const PORT = 5000;
 const app = express();
 const router = express.Router();
+dotenv.config();
 
 app.use(express.static("static"));
 app.use(express.json());
@@ -15,8 +18,20 @@ app.use((_req, res, next) => {
   next();
 });
 
-router.post("/loremIpsum", (req, res) => {
-  res.json({ test: "test" });
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+router.post("/loremIpsum", async (req, res) => {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt:
+      "Napiš mi náhodné věty, které navazují na sebe a oddělují se tečkou ve stylu lorem ipsum, které mají přesně 30 vět.",
+    temperature: 0.5,
+    max_tokens: 1000,
+  });
+  console.log(response.data.choices[0].text);
 });
 
 app.use(router);
